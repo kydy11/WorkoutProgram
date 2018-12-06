0de5665d80d92d3e947c5ca799cc20d6b1e44eda
kydy11 <kyler.day@meritknights.com>
2018-12-06 13:57:47 -0700

    update

---
+++ app/src/main/java/com/example/student/workoutprogram/views/SetEditPage.java
@@ -0,0 +1,17 @@
+package com.example.student.workoutprogram.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.student.workoutprogram.R;
+
+public class SetEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_edit_page);
+
+    }
+
+}
